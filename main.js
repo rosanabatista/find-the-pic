c70d5ba7b337fc762e7b0fbb2d1d90fb84075cf0
Rosana Batista <rosana.psb@gmail.com>
2020-11-03 11:46:52 +0100

    set the enable dark mode

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const maxGameTime = 2;
+const maxGameTime = 20;
 let game = null;
 
 // add logic to start the game
@@ -41,6 +41,31 @@ const backHome = function (event) {
   event.preventDefault();
   userInterface.showInstructions();
 };
+
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
 
 // browser will trigger the event when the dom is ready to be manipulated
 document.addEventListener("DOMContentLoaded", function () {
